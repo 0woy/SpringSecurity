d3d558c76ca53083775ec267547fc1dc0f560d19
0woy <qkrdbsdk1030@naver.com>
2025-06-29 21:55:33 +0900

    feat: hibernate ddl & join 로직 설정

--- src/main/java/com/example/springsecurity/controller/MainController.java
+++ src/main/java/com/example/springsecurity/controller/MainController.java
@@ -1,10 +1,18 @@
 package com.example.springsecurity.controller;
 
+import com.example.springsecurity.dto.JoinDTO;
+import com.example.springsecurity.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired  // 필드 주입 지양...  꼭 생성자 주입으로 사용할 것
+    private JoinService joinService;
+
     @GetMapping("/")
     public String main(){
         return "main";
@@ -18,5 +26,18 @@ public class MainController {
     @GetMapping("/login")
     public String loginP(){
         return "login";
+    }
+
+    @GetMapping("/join")
+    public String joinP(){
+        return "join";
+    }
+
+    @PostMapping("/joinProc")
+    public String joinProcP(JoinDTO joinDTO){
+        System.out.println(joinDTO.getUsername());
+
+        joinService.joinProcess(joinDTO);
+        return "redirect:/login";
     }
 }
---
+++ src/main/java/com/example/springsecurity/service/JoinService.java
@@ -0,0 +1,28 @@
+package com.example.springsecurity.service;
+
+import com.example.springsecurity.dto.JoinDTO;
+import com.example.springsecurity.entity.UserEntity;
+import com.example.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO){
+        UserEntity user = new UserEntity();
+        user.setUsername(joinDTO.getUsername());
+        user.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+        user.setRole("ROLE_USER");
+
+        userRepository.save(user);
+    }
+
+}
